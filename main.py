db14372f969015949beb3a6f5468d2e56adbd007
jhdark <darkj385@gmail.com>
2025-06-05 17:56:44 -0400

    new main script

--- main.py
+++ main.py
@@ -1,63 +1,48 @@
+# main.py
 from shield_das import (
     DataRecorder,
     WGM701_Gauge,
     DataPlotter,
     CVM211_Gauge,
-    Baratron626D_Gauge,
+    Baratron626D_Gauge
 )
-import tkinter as tk
+import time
+import u6
 
+# Create gauges
 gauge_1 = WGM701_Gauge(
-    test_mode=True,
     gauge_location="downstream",
-    export_filename="results/WGM701_pressure_data.csv",
+    export_filename="WGM701_pressure_data.csv",
 )
 gauge_2 = CVM211_Gauge(
-    test_mode=True,
     gauge_location="upstream",
-    export_filename="results/CVM211_pressure_data.csv",
+    export_filename="CVM211_pressure_data.csv",
 )
 gauge_3 = Baratron626D_Gauge(
-    name="Baratron626D_1000T",
-    test_mode=True,
     gauge_location="upstream",
-    export_filename="results/Baratron626D_1000T_pressure_data.csv",
+    export_filename="Baratron626D_1000T_upstream_pressure_data.csv",
 )
 gauge_4 = Baratron626D_Gauge(
-    name="Baratron626D_0.1T",
-    test_mode=True,
     gauge_location="downstream",
-    export_filename="results/Baratron626D_0.1T_pressure_data.csv",
+    export_filename="Baratron626D_1T_downstream_pressure_data.csv",
 )
 
+labjack = u6.U6(firstFound=True)
+
+# Create recorder
 my_recorder = DataRecorder(
     gauges=[gauge_1, gauge_2, gauge_3, gauge_4],
-    labjack=None,
+    labjack=labjack,
 )
 
-
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.title("SHILED DAS")
-    root.geometry("800x600")
-
-    start_button = tk.Button(root, text="Start", command=my_recorder.start)
-    start_button.pack(padx=20, pady=10)
-
-    stop_button = tk.Button(root, text="Stop", command=my_recorder.stop)
-    stop_button.pack(padx=20, pady=10)
-
-    # Live plot inside GUI
-    plotter = DataPlotter(root, my_recorder)
-
-    def on_close():
-        try:
-            my_recorder.stop()  # Stop the data recorder safely
-            # If needed, explicitly stop the animation:
-            plotter.ani.event_source.stop()
-        except Exception as e:
-            print(f"Error during shutdown: {e}")
-        root.destroy()  # Close the GUI window
-
-    root.protocol("WM_DELETE_WINDOW", on_close)
-    root.mainloop()
+    # Create and start the plotter
+    plotter = DataPlotter(my_recorder)
+    plotter.start()
+    
+    # Keep the main thread running
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        my_recorder.stop()
