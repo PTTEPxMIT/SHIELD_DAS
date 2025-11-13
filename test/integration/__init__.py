f9a35cb5ddb0778c7542805a544e0de90f84e7c2
jhdark <james.dark.95@gmail.com>
2025-11-13 14:06:54 -0500

    integration tests for callbacks

---
+++ test/integration/__init__.py
@@ -0,0 +1,6 @@
+"""Integration tests for SHIELD DAS.
+
+This package contains integration tests that test the interaction between
+multiple components, requiring a running Dash application or other complex
+setups that go beyond unit testing.
+"""
